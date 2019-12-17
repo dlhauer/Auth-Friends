9bf636e9b4aa8fdc1c75de4d7cb862a23b74ef57
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2019-12-17 13:11:30 -0800

    Login function

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -15,6 +16,11 @@ function App() {
             <Link to='/protected'>Protected page</Link>
           </li>
         </ul>
+        <Switch>
+          {/* <PrivateRoute path='/protected'/> */}
+          <Route path='/login' component={Login} />
+          <Route component={Login}/>
+        </Switch>
       </div>
     </Router>
   );
--- friends/src/utils/axiosWithAuth.js
+++ friends/src/utils/axiosWithAuth.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export const axiosWithAuth = () => {
   const token = localStorage.getItem('token');
-  axios.create({
-    baseURL: 'http://localhost:5000/api'
+  return axios.create({
+    baseURL: 'http://localhost:5000/api',
     headers: {
       Authorization: token
     }
